36746873ddf0c38906c9a15e50f3b12b8682c867
IgorVash <vppol@rabler.ru>
2022-03-25 09:04:23 +0300

    первая фиксация

---
+++ uch.py
@@ -0,0 +1,8 @@
+# Разбираем ошибки
+import random
+MyVariable = 'Присвоение текста переменной'
+ print(MyName)
+myVariable = random.randint(1,10)
+print('Хотел бы отметить, что '+myVariable+" программированием') 
+myVariable = 'наш 'Центр Компьютерных Технологий' занимается с детьми'
+print('Хотел бы отметить, что "+myVariable+" программированием') 
